a22df0f0fd49fb0ca9539bde189b9f8951f1b415
Daniel Rodriguez <danrodz15.dr@icloud.com>
2018-06-27 22:52:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -72,12 +72,9 @@ def organize_schools(schools)
   organized_schools = {}
   schools.each do |school, hash|
     location = hash[:location]
-    if organized_schools[location]
-      organized_schools[location] << name
-    else
-      organized_schools[location] = []
-      organized_schools[location] << name
-    end
+    organized_schools.has_key?(location) ?
+    organized_schools[location] << school :
+    organized_schools[location] = [school]
   end
   organized_schools
 end
